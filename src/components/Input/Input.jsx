c779dd2689f6b61e9555e34a5315c55c8526049e
Ruslan Volovenko <vokoloven@gmail.com>
2023-07-22 17:06:42 +0300

    Clear register

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -2,11 +2,10 @@
 import TextField from '@mui/material/TextField';
 import { sxInput } from 'theme';
 
-export const Input = ({ input: { id, label }, register }) => {
+export const Input = ({ input: { id, label } }) => {
     return (
         <TextField
             id={id}
-            {...register(id)}
             label={label}
             variant="outlined"
             sx={{ ...sxInput() }}
