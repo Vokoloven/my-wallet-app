8fb04f6ab0c8ed7cbdf610a9b86e8ecb7f47f455
Ruslan Volovenko <vokoloven@gmail.com>
2023-07-22 17:03:34 +0300

    Added comp AppBar

---
+++ src/components/AppBar/AppBar.jsx
@@ -0,0 +1,50 @@
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import LogoDevIcon from '@mui/icons-material/LogoDev';
+import { sxButtonOutlined } from 'theme';
+import { useWeb3Modal } from '@web3modal/react';
+import { useFetchData } from 'hooks';
+import { handleButtonContent } from './handleButtonContent';
+
+export const CustomAppBar = () => {
+    const { open } = useWeb3Modal();
+    const { address, data, isConnected } = useFetchData();
+
+    return (
+        <Box sx={{ flexGrow: 1 }}>
+            <AppBar
+                position="static"
+                sx={{ bgcolor: 'custom.white', boxShadow: 'none' }}
+            >
+                <Toolbar>
+                    <Box sx={{ flexGrow: 1 }}>
+                        <LogoDevIcon
+                            sx={{
+                                width: 40,
+                                height: 40,
+                                color: 'secondary.dark',
+                            }}
+                        />
+                    </Box>
+                    <Button
+                        aria-label="Connect Wallet"
+                        type="button"
+                        variant="outlined"
+                        sx={{ ...sxButtonOutlined(), textTransform: 'none' }}
+                        onClick={() => open()}
+                    >
+                        {handleButtonContent(
+                            isConnected,
+                            address,
+                            Typography,
+                            data
+                        )}
+                    </Button>
+                </Toolbar>
+            </AppBar>
+        </Box>
+    );
+};
---
+++ src/components/AppBar/handleButtonContent.jsx
@@ -0,0 +1,30 @@
+export const handleButtonContent = (isConnected, address, Typography, data) => {
+    const formatAddress = (address) => {
+        if (address) {
+            const start = address.match(/^0x[a-fA-F0-9]{3}/);
+            const end = address.match(/[a-fA-F0-9]{0,4}$/i);
+            return `${start[0]}...${end}`;
+        }
+    };
+
+    const formatBalance = (balance) => {
+        if (balance === '0') {
+            return `${balance}.000`;
+        } else {
+            return balance.match(/\d.{4}/);
+        }
+    };
+
+    if (isConnected) {
+        return (
+            <>
+                <Typography sx={{ mr: 2 }}>
+                    {formatBalance(data.formatted)}
+                </Typography>
+                <Typography>{formatAddress(address)}</Typography>
+            </>
+        );
+    } else {
+        return <Typography>Connect Wallet</Typography>;
+    }
+};
