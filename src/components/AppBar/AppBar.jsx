8c4be8f6060dae6a4a9453ac44ce5811f37ad06c
Ruslan Volovenko <vokoloven@gmail.com>
2023-07-23 18:52:11 +0300

    Added logo for wallet

--- src/components/AppBar/AppBar.jsx
+++ src/components/AppBar/AppBar.jsx
@@ -3,11 +3,11 @@ import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
-import LogoDevIcon from '@mui/icons-material/LogoDev';
 import { sxButtonOutlined } from '../../theme';
 import { useWeb3Modal } from '@web3modal/react';
 import { useFetchData } from '../../hooks';
 import { handleButtonContent } from './handleButtonContent';
+import CardMedia from '@mui/material/CardMedia';
 
 export const CustomAppBar = () => {
     const { open } = useWeb3Modal();
@@ -21,12 +21,10 @@ export const CustomAppBar = () => {
             >
                 <Toolbar>
                     <Box sx={{ flexGrow: 1 }}>
-                        <LogoDevIcon
-                            sx={{
-                                width: 40,
-                                height: 40,
-                                color: 'secondary.dark',
-                            }}
+                        <CardMedia
+                            sx={{ width: 40, height: 40 }}
+                            image="/wallet.png"
+                            title="logo"
                         />
                     </Box>
                     <Button
