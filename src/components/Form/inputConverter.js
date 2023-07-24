cbc25e421276898204fb879b16b8c0661539f551
Ruslan Volovenko <vokoloven@gmail.com>
2023-07-24 18:23:26 +0300

    Added fix for regexp

--- src/components/Form/inputConverter.js
+++ src/components/Form/inputConverter.js
@@ -2,15 +2,21 @@ export const inputConverter = (id, value, setValue) => {
     if (id === 'address') {
         const address = value
             .replace(/[^\w]/gi, '')
-            .match(/^(0x)([a-fA-F0-9]{0,40})/);
-        const reAddress = '0x' + (address?.[2] ? address?.[2] : '');
+            .match(/(?:0x?)([a-fA-F0-9]{0,40})/);
+
+        console.log(address);
+        const reAddress = address?.[0] ? address?.[0] : '';
         setValue('address', reAddress);
     } else if (id === 'balance') {
-        const balance = value.match(/^(100000|[0-9]\d{0,4})(?:\.\d{1,6})?$/);
+        const balance = value.match(
+            /^100000|((?:[1-9][0-9]{0,4}(?:\.[0-9]{0,6})?)|(?:[1-9][0-9]{0,4})|(?:0\.[0-9]{0,6})|0)/
+        );
 
         console.log(balance);
 
-        // const reBalance =
-        //     (balance?.[0] ? balance?.[0] : '') + (balance?.[2] ? '' : '.');
+        const reBalance = balance?.[0] ? balance?.[0] : '';
+
+        //+ (balance?.[2] ? '' : '.');
+        setValue('balance', reBalance);
     }
 };
